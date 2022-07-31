49083f79aaf9fcb05e58986e21cdfa7eea49559b
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-01 00:22:45 +0500

    hello

--- bin/if02.dart
+++ bin/if02.dart
@@ -8,5 +8,27 @@ Args:
 Returns:
     int: return answer.
 */
+
+int func(int a, int b, int c) {
+    int g =0;
+    if (a<b) {
+        if (a<c) {
+            return g = a;
+        }
+    }
+    else if (b<a) {
+        if (b<c) {
+            return g = b;
+        }
+    }
+    else if (c<a) {
+        if (c<b) {
+            return g = c;
+        }
+    }
+    return g;
+}
+
 void main() {
+    print(func(-9,2,3));
 }
